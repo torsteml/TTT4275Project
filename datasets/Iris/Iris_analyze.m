4e587434493944d6028de8b041e9b370ef5851ba
Fossenkall <martilim@stud.ntnu.no>
2019-03-25 11:38:43 +0100

    Move testing measures into separate function.

--- datasets/Iris/Iris_analyze.m
+++ datasets/Iris/Iris_analyze.m
@@ -82,28 +82,10 @@ FN = zeros(C,1);
 for c = 1:C
    FN(c) = nnz(classified_classes(ground_truth==c)~=c);
 end
-% True positive rate, sensitivity
-TPR = TP./(TP+FN);
-% True negative rate, specificity
-TNR = TN./(TN+FP);
-% Positive preditictive rate, precision
-PPV = TP./(TP+FP);
-% Negative preditictive rate
-NPV = TN./(TN+FN);
-% False negative rate, miss rate
-FNR = 1-TPR;
-% False positive rate, fall-out
-FPR = 1-TNR;
-% False discovery rate
-FDR = 1-PPV;
-% False ommission rate
-FOR = 1-NPV;
-% Accuracy
-ACC = (TP+TN)./(TP+TN+FP+FN);
-% F1 score
-%  - The harmonic mean of precision and sensitivity
-F1 = 2.*(PPV.*TPR)./(PPV+TPR);
-
+[TPR, TNR, PPV, NPV, FNR, FPR, FDR, FOR, ACC, F1] =...
+    calculate_testingMeasures(TP, TN, FP, FN);
+[TPR_tot, TNR_tot, PPV_tot, NPV_tot, FNR_tot, FPR_tot, FDR_tot, FOR_tot, ACC_tot, F1_tot] =...
+    calculate_testingMeasures(sum(TP), sum(TN), sum(FP), sum(FN));
 
 %% Show results W_all(2,1,:)
 x1 = [1:train_iterations];
