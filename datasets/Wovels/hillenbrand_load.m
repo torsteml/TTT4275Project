f54fb63f89ac8972c7c1dcebb5ebf8a1d3e96075
Fossenkall <martilim@stud.ntnu.no>
2019-04-01 10:35:22 +0200

    2b) add prediction code.

--- datasets/Wovels/hillenbrand_load.m
+++ datasets/Wovels/hillenbrand_load.m
@@ -3,7 +3,8 @@
 formant_table = load_vowdata();
 
 %% 1a) Find the sample mean and the sample covariance matrix for each class set.
-
+train_len = 70;
+test_len = 69;
 % # Seperate into each vowel class
 vowels = unique(formant_table.vowel);
 vowel_classes = cell(numel(vowels),3);
@@ -12,8 +13,8 @@ vowel_classes_test = cell(numel(vowels),3);
 
 for row = 1:numel(vowels)
    vowel_classes{row,1} = formant_table(formant_table.vowel == vowels(row),:);
-   vowel_classes_train{row,1} = vowel_classes{row,1}(1:70,:)
-   vowel_classes_test{row,1} = vowel_classes{row,1}(71:139,:)
+   vowel_classes_train{row,1} = vowel_classes{row,1}(1:70,:);
+   vowel_classes_test{row,1} = vowel_classes{row,1}(71:139,:);
 end
 
 % ## Find mean and covariance for each vowel
@@ -27,19 +28,29 @@ end
 %% 1b) Create single Gaussian class model for each class. 
 gaussian_models = cell(numel(vowels),1);
 for row = 1:numel(vowels)
-    mu=vowel_classes{row,2}; 
-    sigma=vowel_classes{row,3};
+    mu=vowel_classes_train{row,2}; 
+    sigma=vowel_classes_train{row,3};
     gaussian_models{row} = gmdistribution(mu,sigma);
 end
 
 %% Classify training set
-training_set = vec;
-class_probabilities = zeros(size(training_set,1),numel(vowels));
-
-for vowelNum = 1:numel(vowels)
-    class_probabilities(:,vowelNum) = pdf(gaussian_models(vowel_num,1));
-    
+% Create test matrix where each column is F0-F4
+testing_set = zeros(test_len*numel(vowels),4);
+for vowel_num = 1:numel(vowels)
+    tbl = vowel_classes_test{vowel_num,1};
+   testing_set(1+test_len*(vowel_num-1):(test_len*vowel_num),:) = ...
+       [tbl.F0s, tbl.F1s, tbl.F2s, tbl.F3s]; 
 end
+% Classify
+class_probabilities = zeros(size(testing_set,1),numel(vowels));
+
+for vowel_num = 1:numel(vowels)
+    class_probabilities(:,vowel_num) = pdf(gaussian_models{vowel_num,1},testing_set);
+end
+
+[~, predicted_classes] = max(class_probabilities,[],2);
+
+%% Evaluate errors. 
 
 
 
