00ef393ae25bdc6c5ed439042afdff77c6e3f883
Fossenkall <martilim@stud.ntnu.no>
2019-04-01 09:03:37 +0200

    Oppgave 1a

--- datasets/Wovels/hillenbrand_load.m
+++ datasets/Wovels/hillenbrand_load.m
@@ -1,6 +1,28 @@
 %% Hillenbrand Load
 
 formant_table = load_vowdata();
+
+%% 1a) Find the sample mean and the sample covariance matrix for each class set.
+
+% # Seperate into each vowel class
+vowels = unique(formant_table.vowel);
+vowel_classes = cell(numel(vowels),3); % data, mean, covariance
+for row = 1:numel(vowels)
+   vowel_classes{row,1} = formant_table(formant_table.vowel == vowels(row),:); 
+end
+
+% ## Find mean for each vowel
+for row = 1:numel(vowels)
+    tbl = vowel_classes{row,1};
+    formant_matrix = [tbl.F0s, tbl.F1s, tbl.F2s, tbl.F3s];
+    vowel_classes{row,2} = mean(formant_matrix); 
+    vowel_classes{row,3} = cov(formant_matrix); 
+end
+
+
+
+
+
 %% Make scatter plots
 
 
